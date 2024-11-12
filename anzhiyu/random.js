bd92c35cace8e691d41bb4794cc1cc11e2122053
tianhe2005 <1371453554@qq.com>
2024-11-12 23:57:57 +0800

    Site updated: 2024-11-12 23:57:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/11/hello-world/","2024/11/12/如何自己部署一个IDE/"];function toRandomPost(){
+var posts=["2024/11/11/hello-world/","2024/11/12/如何自己部署一个IDE/","2024/11/12/2024年9月日常/","2024/11/12/大一总结/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
