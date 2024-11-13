91d05cbdd819a7e754d1828b39aa1a7db49d0c99
tianhe2005 <1371453554@qq.com>
2024-11-13 19:21:11 +0800

    Site updated: 2024-11-13 19:21:11

---
+++ album/index.md
@@ -0,0 +1,7 @@
+---
+title: 相册集
+date: 2024-11-12 21:10:26
+aside: false
+top_img: false
+type: "album"
+---
--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/11/hello-world/","2024/11/12/如何自己部署一个IDE/","2024/11/12/2024年9月日常/","2024/11/12/大一总结/"];function toRandomPost(){
+var posts=["2024/11/12/hello-world/","2024/11/12/如何自己部署一个IDE/","2024/11/12/2024年9月日常/","2024/11/12/大一总结/","2024/11/12/数据库底层原理（1）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
